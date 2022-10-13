b973866c73a274e4e7d8015662a0d3326711b893
Артюшенко Александр <zasranechamozur@gmail.com>
2022-10-13 22:53:43 +0300

    3

---
+++ task3/Program.cs
@@ -0,0 +1,33 @@
+﻿/* Напишите программу, которая принимает на вход число (N)
+ и выдаёт таблицу кубов чисел от 1 до N.
+ 3 -> 1, 8, 27
+5 -> 1, 8, 27, 64, 125
+ */
+
+int Prompt(string message)
+{
+    Console.Write(message);
+    int number = Convert.ToInt32(Console.ReadLine());
+    return number;
+}
+
+bool ValidateNumber(int number)
+{
+    if (number < 1)
+    {
+        Console.WriteLine("Ой! Вы ввели не натуральное число.");
+        return false;
+    }
+    return true;
+}
+
+int num = Prompt("Введите натуральное число: ");
+
+if(ValidateNumber(num))
+{
+    Console.WriteLine($"Кубы чисел от 1 до {num}: ");
+    for (int i = 1; i <= num; i ++)
+    {
+        Console.Write(Math.Pow(i, 3) + " | ");
+    }
+}
