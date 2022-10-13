98adcb0a65720bb1635ebcb018991705dfe5c061
Артюшенко Александр <zasranechamozur@gmail.com>
2022-10-13 21:53:22 +0300

    first

---
+++ task1/Program.cs
@@ -0,0 +1,42 @@
+﻿
+/* Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+14212 -> нет
+
+12821 -> да
+
+23432 -> да
+*/
+int Prompt(string message)
+{
+    Console.Write(message);
+    int number = Convert.ToInt32(Console.ReadLine());
+    return number;
+}
+
+bool ValidateNumber(int number)
+{
+    if (number < 10000 || number > 99999)
+    {
+        Console.WriteLine("Ой! Вы ввели не пятизначное число.");
+        return false;
+    }
+    return true;
+}
+
+string CheckPalindromes(int palindrome)
+{
+    if
+    (
+        (palindrome / 10000 == palindrome % 10) && (palindrome / 1000 % 10 == palindrome % 100 / 10)
+    )
+    {
+        return "Это число палиндром!";
+    }
+    return "Это число не является палиндромом!";
+}
+
+int checkNumber = Prompt("Введите пятизначное число: ");
+if (ValidateNumber(checkNumber))
+{
+    Console.WriteLine(CheckPalindromes(checkNumber));
+}
